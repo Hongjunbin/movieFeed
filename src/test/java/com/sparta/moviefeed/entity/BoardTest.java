4f18c3d6892774ec9583059ff40abca748246321
junbin <ghdwnsqls0@naver.com>
2024-06-15 00:24:10 +0900

    AOP 추가 기능 구현

---
+++ src/test/java/com/sparta/moviefeed/entity/BoardTest.java
@@ -0,0 +1,26 @@
+//package com.sparta.moviefeed.entity;
+//
+//import com.sparta.moviefeed.repository.BoardRepository;
+//import com.sparta.moviefeed.repository.UserRepository;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class BoardTest {
+//
+//    @Mock
+//    BoardRepository boardRepository;
+//
+//    @Mock
+//    UserRepository userRepository;
+//
+//    @Test
+//    void boardTest() {
+//        Board board = new Board();
+//        User user = new User();
+//        boardRepository.findById()
+//    }
+//
+//}
